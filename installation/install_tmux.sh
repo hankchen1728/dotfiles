79eb4983b0c2d4e21ad7e53db28423f68595b7e9
hankchen1728 <hankchen1728@gmail.com>
2021-02-13 23:06:16 +0800

    Update tmux installer

--- installation/install_tmux.sh
+++ installation/install_tmux.sh
@@ -10,6 +10,7 @@ cd tmux
 bash autogen.sh
 # Configuration and Installation
 ./configure --prefix ${HOME}/.local/ && make -j 8
+make install -j 8
 
 # For bash alias
 echo "alias tmux="${HOME}/.local/bin/tmux"" >> ${HOME}/.config/bash/.bashrc
