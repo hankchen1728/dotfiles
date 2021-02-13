31dfa7da015c231b4380e119720d0b6f0a464081
hankchen1728 <hankchen1728@gmail.com>
2021-02-14 00:03:39 +0800

    Update fish config file

--- fish/config.fish
+++ fish/config.fish
@@ -4,7 +4,8 @@ alias ll="ls -alhF"
 alias la="ls -A"
 
 # Environment Module
-set ModuleFish "/usr/share/modules/init/fish"
+# set ModuleFish "/usr/share/modules/init/fish"
+set ModuleFish "$MODULESHOME"/init/fish
 if test -f $ModuleFish
     source $ModuleFish
 end
